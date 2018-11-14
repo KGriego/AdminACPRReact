01ffbe62d4cde9f8d2e2fb1739cb658776b78828
Kevin Griego <kev.gri32@gmail.com>
2018-11-14 11:29:15 -0700

    trying to add react paths

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/apiRoutes");
@@ -11,6 +12,13 @@ const PORT = process.env.PORT || 3010;
 // ===== Middleware ====
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.use("/api", apiRoutes);
 

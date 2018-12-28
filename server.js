7140d00da970049fbfb58c2ccafdb2ec65f7c155
Kevin Griego <kev.gri32@gmail.com>
2018-12-28 14:36:15 -0700

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const cors = require("cors");
 
 const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/apiRoutes");
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3010;
 // ===== Middleware ====
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors);
 app.use("/api", apiRoutes);
 
 app.listen(PORT, function() {
